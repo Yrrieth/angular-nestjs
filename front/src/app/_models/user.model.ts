360a7ce78dd2711e05045c5e10e14ed1f2b30f50
Yrrieth <thierry.wen@laposte.net>
2020-01-06 21:50:03 +0100

    Modifié affichage de la BDD home/

--- front/src/app/_models/user.model.ts
+++ front/src/app/_models/user.model.ts
@@ -1,4 +1,6 @@
 export class UserModel {
+  userId: number;
   username: string;
   password: string;
+  salt: number;
 }
--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 import { first } from 'rxjs/operators';
 import { UserService } from '../_services/user.service';
-
-//import { UserService, AuthenticationService } from '@app/_services';
+import { AuthenticationService } from '../_services/authentication.service';
+import { UserModel } from '../_models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +13,7 @@ import { UserService } from '../_services/user.service';
 export class HomeComponent implements OnInit {
   routeHidden = true;
   loading = false;
-  //users: User[];
+  users: UserModel[];
 
   constructor(
     private router: Router,
@@ -31,8 +31,10 @@ export class HomeComponent implements OnInit {
       }
     })
 
-    this.user.getAll().subscribe((res) => {
+    this.user.getAll().subscribe((res: UserModel[]) => {
       console.log(res);
+      this.loading = false;
+      this.users = res;
     })
   }
 
